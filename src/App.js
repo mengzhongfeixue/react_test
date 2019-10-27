285688dba626070089a21f4318f044768ca5180c
mengzhongfeixue <542061851@qq.com>
2019-10-27 12:48:35 +0800

    安装antd并配置及主题修改

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './index.less'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
